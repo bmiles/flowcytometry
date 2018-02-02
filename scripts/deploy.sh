1544a7da22a9e5c03d0af3daac73a4547c4cfc7c
Ben Miles <ben.miles2@gmail.com>
2018-02-01 22:52:47 -0600

    tilda dot

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ source ~/virtualenv/python2.7/bin/activate
 pip install transcriptic
 cp scripts/.transcriptic ~/.transcriptic
 cat ~/.transcriptic
-cat ~/.transcriptic | sed "s/\$EMAIL/$email/g" > ~./transcriptic
+cat ~/.transcriptic | sed "s/\$EMAIL/$email/g" > ~/.transcriptic
 cat ~/.transcriptic
-cat ~/.transcriptic | sed "s/\$TOKEN/$token/g" > ~./transcriptic
+cat ~/.transcriptic | sed "s/\$TOKEN/$token/g" > ~/.transcriptic
 cat ~/.transcriptic
-cat ~/.transcriptic | sed "s/\$ORGANIZATION_ID/$organization_id/g" > ~./transcriptic
+cat ~/.transcriptic | sed "s/\$ORGANIZATION_ID/$organization_id/g" > ~/.transcriptic
 cat ~/.transcriptic
-cat ~/.transcriptic | sed "s/\$USER_ID/$user_id/g" > ~./transcriptic
+cat ~/.transcriptic | sed "s/\$USER_ID/$user_id/g" > ~/.transcriptic
 cat ~/.transcriptic
 transcriptic packages
 transcriptic build-release
