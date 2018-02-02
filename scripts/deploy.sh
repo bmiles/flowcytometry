828563a5746c45e76705b37c191fd6f4c1dd839c
Ben Miles <ben.miles2@gmail.com>
2018-02-01 21:59:02 -0600

    single script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "hi I'm the start $PWD"
+env | sort
+cp scripts/.transcriptic ~/.transcriptic
+pipenv install
+transcriptic packages
+transcriptic build-release
+transcriptic upload-release release.zip pk1b6etjw8x2ep
+echo "hi I'm the end"
