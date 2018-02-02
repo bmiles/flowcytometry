553780d495ab998ae4e0daea3b30f35ff81efcb3
Ben Miles <ben.miles2@gmail.com>
2018-02-02 00:07:30 -0600

    proj

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ sed -i "s/\$FEATURE1/$feature1/g" ~/.transcriptic
 cat ~/.transcriptic
 sed -i "s/\$FEATURE2/$feature2/g" ~/.transcriptic
 cat ~/.transcriptic
-transcriptic runs
+transcriptic projects
 transcriptic packages
 transcriptic build-release
 transcriptic upload-release release.zip pk1b6etjw8x2ep
