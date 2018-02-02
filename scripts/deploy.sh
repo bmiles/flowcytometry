010c8bbc48229f661527a96adaeb69aa45109f28
Ben Miles <ben.miles2@gmail.com>
2018-02-02 11:10:48 -0600

    added badge

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,11 @@
 set -x
 pip install transcriptic
 cp scripts/.transcriptic ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$EMAIL/$email/g" ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$TOKEN/$token/g" ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$ORGANIZATION_ID/$organization_id/g" ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$USER_ID/$user_id/g" ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$FEATURE1/$feature1/g" ~/.transcriptic
-cat ~/.transcriptic
 sed -i "s/\$FEATURE2/$feature2/g" ~/.transcriptic
 cat ~/.transcriptic
 transcriptic --config ~/.transcriptic projects
