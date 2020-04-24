532ad3658c34ae95b70fefbe41f6affcf054c124
bmiles <ben.miles2@gmail.com>
2020-04-24 00:26:53 -0700

    test: make samples length

--- flow_cytometry_test.py
+++ flow_cytometry_test.py
@@ -1,9 +1,11 @@
+from flow_cytometry import make_samples
+
 def inc(x):
     return x + 1
 
 
-def test_answer_fail():
-    assert inc(3) != 5
+def test_make_samples():
+    assert len(make_samples([0,1,2,3], 10)) == 4
 
 
 def test_answer_pass():
